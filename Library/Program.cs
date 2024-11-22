cf05abf8ac41c400af724ee38905b6ef6bbe9041
Eldgeron <ganna.liubimova@gmail.com>
2024-11-22 20:12:25 +0200

    + src/Library

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,25 +1,41 @@
+using Library.Database;
+using Library.Endpoints.Books;
+using Library.Endpoints.User;
+using Library.Options;
+using Library.Repositories;
+using Library.Services;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", false)
+    .Build();
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom
+    .Configuration(builder.Configuration)
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Host.UseSerilog();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<MongoDbOptions>(builder.Configuration.GetSection("MongoDbOptions"));
+builder.Services.AddSingleton<IMongoDbConnectionFactory, MongoDbFactory>();
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<IAuthorizationTokenRepository, AuthorizationTokenRepository>();
+builder.Services.AddSingleton<IBookRepository, BookRepository>();
+builder.Services.AddSingleton<IUserAuthorizationService, UserAuthorizationService>();
+
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.MapCreateUser();
+app.MapLogIn();
+app.MapCreateBook();
+app.MapGetBooksByTitle();
+app.MapGetBooksByAuthor();
+app.MapDeleteBook();
 app.Run();
